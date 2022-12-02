7e097e75c7f26843ecd68b915e0acf3e0df3d84d
Ankush <ankushtsalian@gmail.com>
2022-12-02 17:53:25 +0530

    created and refractered code in db with mongoose setup

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
+//DataBase
 require("./DataBase/connect");
 const express = require("express");
 const connectDB = require("./DataBase/connect");
 const app = express();
+
+//.env
+require("dotenv").config();
 const tasks = require("./Router/tasks");
 const Port = 5000;
-// app.use(express.static("./public"));
 
+/*--------------------MIDDLEWARE---------------------- */
+
+// app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: false }));
 // app.use(cors());
 app.use(express.json());
-
 app.use("/api/v1/tasks", tasks);
+
+/*--------------------MIDDLEWARE---------------------- */
 
 app.all("*", (req, res) => {
   res.send("<h1>404 Not found</h1>");
 });
 
+/*----------------------DB  and Server setup---------------------------------- */
+
 const start = async () => {
   try {
-    await connectDB();
+    await connectDB(process.env.MONGO_URI);
     app.listen(Port, () => console.log(`Server is listening in Port ${5000}`));
   } catch (error) {
     console.log(error);
@@ -26,3 +35,4 @@ const start = async () => {
 };
 
 start();
+/*----------------------DB  and Server setup---------------------------------- */
