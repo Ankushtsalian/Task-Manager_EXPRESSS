d39909a135f3e94baa526a7c63a8d9e7c7f8b206
Ankush <ankushtsalian@gmail.com>
2022-12-02 11:37:25 +0530

    created initial server setup

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const Port = 5000;
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  return res.send("Task Manager App");
+});
+
+app.listen(Port, () => console.log(`Server is listening in Port ${5000}`));
